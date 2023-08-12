e16cd344cc7104d26faabf79b66f752ff63bb683
Elijah Nikov <posnikovelijah@gmail.com>
2023-08-13 00:07:08 +0100

    adding calendar and date picker components, rating component, finalising form for creating new review

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -6,7 +6,7 @@ import classNames from "classnames";
 import React from "react";
 import { type IconType } from "react-icons";
 
-const button = cva(
+export const buttonVariants = cva(
     [
         "inline-flex items-center justify-center rounded-lg font-medium",
         "focus:outline-none focus-visible:ring focus-visible:ring-crumble-500",
@@ -74,7 +74,7 @@ const button = cva(
 
 interface ButtonProps
     extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "disabled">,
-        VariantProps<typeof button> {
+        VariantProps<typeof buttonVariants> {
     children?: React.ReactNode;
     loading?: boolean;
     leftIcon?: IconType;
@@ -101,7 +101,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             <button
                 ref={ref}
                 {...props}
-                className={button({
+                className={buttonVariants({
                     intent,
                     size,
                     fullWidth,
