c3a8950c3e13fbb90cb519c9664a3f52f665c14c
Elijah Nikov <posnikovelijah@gmail.com>
2023-08-20 23:36:46 +0100

    extracting navigation bar array

--- src/utils/fetch/zodFetch.ts
+++ src/utils/fetch/zodFetch.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { env } from "@/env.mjs";
-import { z } from "zod";
+import type { z } from "zod";
 
 export const zodFetch = async <T extends z.Schema>(url: string, schema: T) => {
     const res = await fetch(url, {
