860794f60c42e8b70e3e52f92e7b17117a33bfe6
elijahnikov <enkv@pm.me>
2023-09-02 23:02:41 +0100

    setting up initial list page

--- src/pages/list/[id].tsx
+++ src/pages/list/[id].tsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/common/Layout/Layout";
 import { generateSSGHelper } from "@/server/helpers/ssgHelper";
 import { api } from "@/utils/api";
 import {
@@ -6,6 +7,8 @@ import {
     InferGetStaticPropsType,
     NextPage,
 } from "next";
+import Head from "next/head";
+import toast from "react-hot-toast";
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -13,8 +16,59 @@ const SingleListPage: NextPage<PageProps> = ({ id }) => {
     const trpcUtils = api.useContext();
 
     const { data, isLoading } = api.list.list.useQuery({ id });
+    const {
+        data: listComments,
+        isLoading: isListCommentsLoading,
+        isError: isListCommentsError,
+        hasNextPage,
+        fetchNextPage,
+    } = api.list.infiniteCommentFeed.useInfiniteQuery(
+        {
+            limit: 10,
+            id: data!.list.id!,
+        },
+        {
+            getNextPageParam: (lastPage) => lastPage.nextCursor,
+        }
+    );
 
-    return <></>;
+    const { mutate: createListComment } =
+        api.list.createListComment.useMutation({
+            onSuccess: async () => {
+                await trpcUtils.list.infiniteCommentFeed.invalidate();
+            },
+        });
+    const { mutate: deleteListComment } =
+        api.list.deleteListComment.useMutation({
+            onSuccess: async () => {
+                toast.success(`Deleted your comment.`, {
+                    position: "bottom-center",
+                    duration: 4000,
+                    className: "dark:bg-brand dark:text-white text-black",
+                });
+                await trpcUtils.list.infiniteCommentFeed.invalidate();
+            },
+        });
+    const toggleLike = api.list.toggleListCommentLike.useMutation({
+        onSuccess: async () => {
+            await trpcUtils.list.infiniteCommentFeed.invalidate();
+        },
+    });
+
+    if (isLoading) return <div>Loading...</div>;
+
+    if (!data) return <div>404</div>;
+
+    return (
+        <>
+            <Head>
+                <title>{`@${data.list.title} - List by @${data.list.user.name} • Crumble`}</title>
+            </Head>
+            <Layout>
+                <h1>test</h1>
+            </Layout>
+        </>
+    );
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
